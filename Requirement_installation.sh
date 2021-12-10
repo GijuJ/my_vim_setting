8aa353363a2b24f5388109b19ff3f5cbe9ae87d2
GijuJ <asgktka@gmail.com>
2021-12-10 12:16:45 +0900

    Update Requirement_installation.sh

--- Requirement_installation.sh
+++ Requirement_installation.sh
@@ -8,8 +8,10 @@ sudo apt install vim
 sudo apt install git
 sudo apt install wget
 
-# if you want to make your configuration by this script only, use this below comment.
+# if you want to make your configuration by this script only, use this below comments.
 # git clone https://github.com/GijuJ/my_vim_setting.git ~/
+# cp my_vim_setting/.vimrc .
+# cp -r my_vim_setting/.vim .
 
 # fzf install
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
